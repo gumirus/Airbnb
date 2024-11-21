c8801b48f8eefaf66141a4972f8f147fdfe39f88
Ruslan Gumirov <gumirus29@gmail.com>
2024-11-21 08:22:03 +0500

    fix: vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Airbnb/', // Change this to match your repository name
   plugins: [react()],
-  build: {
-    outDir: 'build',
-  },
 });
+
